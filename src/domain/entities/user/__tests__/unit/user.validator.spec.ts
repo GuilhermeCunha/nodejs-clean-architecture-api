d5504ea08124d7a6ccd71fc1935f936867527f97
Guilherme Cunha <guiscunha@gmail.com>
2022-09-15 15:39:19 -0300

    feat(UserValidator): improving username validations

--- src/domain/entities/user/__tests__/unit/user.validator.spec.ts
+++ src/domain/entities/user/__tests__/unit/user.validator.spec.ts
@@ -1,26 +1,74 @@
 import { UserValidator } from '../../user.validator'
+import { userPropsFixture } from '../fixtures/user.props.fixture'
 
 describe('UserValidator', () => {
-    describe('validate', () => {
-        it('should return one error if username is not valid', () => {
+    describe('isAlphanumericOnly', () => {
+        it('should return true if text is alphanumeric', () => {
             const validator = new UserValidator()
-            const invalidUsernames = ['', null, undefined]
+            const input = 'Ac123'
+            expect(validator.isAlphanumericOnly(input)).toBe(true)
+        })
 
-            invalidUsernames.forEach((username) => {
-                const errors = validator.validate({
-                    username,
-                    createdAt: new Date(),
-                    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-                } as any)
+        it('should return true if text has symbols', () => {
+            const validator = new UserValidator()
+            const input = 'abc123!'
+            expect(validator.isAlphanumericOnly(input)).toBe(false)
+        })
+    })
+    describe('validate', () => {
+        describe('username', () => {
+            it('should return error if username is not string', () => {
+                const validator = new UserValidator()
+                const invalidUsernames = [null, undefined, true, 100]
+
+                // eslint-disable-next-line @typescript-eslint/no-explicit-any
+                invalidUsernames.forEach((username: any) => {
+                    const errors = validator.validate(
+                        userPropsFixture({
+                            username,
+                        })
+                    )
+                    expect(errors).toContainEqual(
+                        expect.stringContaining(`'username' should be a string`)
+                    )
+                })
+            })
+            it('should return error if username is empty', () => {
+                const validator = new UserValidator()
+                const username = ''
+
+                const errors = validator.validate(
+                    userPropsFixture({
+                        username,
+                    })
+                )
                 expect(errors).toContainEqual(
-                    expect.stringContaining('username')
+                    expect.stringContaining(
+                        `'username' should have at least one character`
+                    )
+                )
+            })
+            it('should return error if username is greater than 14', () => {
+                const validator = new UserValidator()
+                const username = 'greaterthan14charactersusername'
+
+                const errors = validator.validate(
+                    userPropsFixture({
+                        username,
+                    })
+                )
+                expect(errors).toContainEqual(
+                    expect.stringContaining(
+                        `'username' should have at most 14 characters`
+                    )
                 )
             })
         })
+
         it('should  dont return errors if input is valid', () => {
             const validator = new UserValidator()
             const errors = validator.validate({
-                username: 'Valid Name',
+                username: 'Valid',
                 createdAt: new Date(),
                 // eslint-disable-next-line @typescript-eslint/no-explicit-any
             } as any)
--- src/domain/entities/user/user.validator.ts
+++ src/domain/entities/user/user.validator.ts
@@ -2,11 +2,27 @@ import { IValidator } from '../../ports/validator'
 import { UserProps } from './user.props'
 
 export class UserValidator implements IValidator<UserProps> {
+    isAlphanumericOnly(text: string) {
+        return new RegExp(/^[a-z0-9]+$/i).test(text)
+    }
+
     validate(input: UserProps): string[] {
         const errors: string[] = []
 
-        if (!input.username) {
-            errors.push(`Invalid 'username'.`)
+        if (typeof input.username !== 'string') {
+            errors.push(`'username' should be a string`)
+        } else {
+            if (input.username.length < 1) {
+                errors.push(`'username' should have at least one character`)
+            }
+
+            if (input.username.length > 14) {
+                errors.push(`'username' should have at most 14 characters`)
+            }
+
+            if (!this.isAlphanumericOnly(input.username)) {
+                errors.push(`'username' should be alphanumeric characters only`)
+            }
         }
 
         return errors
