641739fa0be6ad00cc22f1f4bb863774de1d5912
Guilherme Cunha <guilherme.s.cunha@accenture.com>
2022-09-19 19:00:39 -0300

    feat: adding typeorm post repository

--- src/domain/ports/repositories/post-repository.ts
+++ src/domain/ports/repositories/post-repository.ts
@@ -1,7 +1,36 @@
 import { PostProps } from '../../entities/post/post.props'
 
+export type GetPosts = {
+    filters?: PostFilters
+    pagination: GetPostsPagination
+}
+
+export type PostFilters = {
+    authorId?: string
+    createdAfter?: Date
+    createdBefore?: Date
+}
+
+export type GetPostsPagination = {
+    skip: number
+    limit: number
+}
+
+export type GetPostsResponsePagination = {
+    total: number
+    skip: number
+    limit: number
+}
+
+export type GetPostsResponse = {
+    posts: PostProps[]
+    pagination: GetPostsResponsePagination
+}
+
 export interface IPostRepository {
     createPost(post: PostProps): Promise<PostProps>
     countPostsByUserInADay(userId: string, day: Date): Promise<number>
     getPostById(postId: string): Promise<PostProps | null>
+    getPosts(param: GetPosts): Promise<GetPostsResponse>
+    countPosts(filters?: PostFilters): Promise<number>
 }
--- src/infrastructure/db/typeorm/repositories/post.repository.ts
+++ src/infrastructure/db/typeorm/repositories/post.repository.ts
@@ -1,7 +1,13 @@
 import { endOfDay, startOfDay } from 'date-fns'
-import { Between } from 'typeorm'
+import { isNil, omitBy } from 'lodash'
+import { Between, LessThan, MoreThan } from 'typeorm'
 import { PostProps } from '../../../../domain/entities/post/post.props'
-import { IPostRepository } from '../../../../domain/ports/repositories/post-repository'
+import {
+    GetPosts,
+    GetPostsResponse,
+    IPostRepository,
+    PostFilters,
+} from '../../../../domain/ports/repositories/post-repository'
 import { PostEntity } from '../entities/Post'
 import { TypeORMDatabase } from '../typeorm-database'
 
@@ -10,6 +16,60 @@ export class TypeormPostRepository implements IPostRepository {
     constructor(database: TypeORMDatabase) {
         this.database = database
     }
+
+    parseFilters(filters: PostFilters) {
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+        const parsedFilters: any = {
+            createdAt: undefined,
+            authorId: filters.authorId,
+        }
+
+        if (filters.createdAfter && filters.createdBefore) {
+            parsedFilters.createdAt = Between(
+                filters.createdAfter,
+                filters.createdBefore
+            )
+        } else if (filters.createdAfter) {
+            parsedFilters.createdAt = MoreThan(filters.createdAfter)
+        } else if (filters.createdBefore) {
+            parsedFilters.createdAt = LessThan(filters.createdBefore)
+        }
+
+        return omitBy(parsedFilters, isNil)
+    }
+    async getPosts({
+        filters = {},
+        pagination,
+    }: GetPosts): Promise<GetPostsResponse> {
+        const parsedFilters = this.parseFilters(filters)
+
+        const database = await this.database.getDatabase()
+        const repository = database.getRepository(PostEntity)
+
+        const [posts, total] = await repository.findAndCount({
+            where: parsedFilters,
+            skip: pagination.skip,
+            take: pagination.skip,
+        })
+        return {
+            posts: posts as PostProps[],
+            pagination: {
+                ...pagination,
+                total,
+            },
+        }
+    }
+    async countPosts(filters: PostFilters): Promise<number> {
+        const parsedFilters = this.parseFilters(filters)
+
+        const database = await this.database.getDatabase()
+        const repository = database.getRepository(PostEntity)
+
+        return repository.count({
+            where: parsedFilters,
+        })
+    }
+
     async createPost(post: PostProps): Promise<PostProps> {
         const database = await this.database.getDatabase()
         const repository = database.getRepository(PostEntity)
@@ -19,6 +79,7 @@ export class TypeormPostRepository implements IPostRepository {
 
         return savedEntity as PostProps
     }
+
     async countPostsByUserInADay(authorId: string, day: Date): Promise<number> {
         const database = await this.database.getDatabase()
         const repository = database.getRepository(PostEntity)
@@ -30,6 +91,7 @@ export class TypeormPostRepository implements IPostRepository {
             },
         })
     }
+
     async getPostById(postId: string): Promise<PostProps | null> {
         const database = await this.database.getDatabase()
         const repository = database.getRepository(PostEntity)
