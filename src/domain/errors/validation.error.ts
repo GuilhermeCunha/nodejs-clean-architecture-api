a74f879a7e66a181fc2ccfb4e213577949ee0839
Guilherme Cunha <guilherme.s.cunha@accenture.com>
2022-09-19 19:23:56 -0300

    feat: adding array of errors in ValidationError

--- src/domain/errors/validation.error.ts
+++ src/domain/errors/validation.error.ts
@@ -1,5 +1,7 @@
 export class ValidationError extends Error {
+    errors: string[]
     constructor(errors: string[]) {
         super(JSON.stringify(errors))
+        this.errors = errors
     }
 }
