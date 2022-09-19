8b6b20b28353cd55ecf7e4370d47145c1031bfd1
Guilherme Cunha <guilherme.s.cunha@accenture.com>
2022-09-19 19:24:32 -0300

    feat: adding post create router

---
+++ src/infrastructure/presentation/http/controllers/posts/posts.controller.ts
@@ -0,0 +1,45 @@
+import { Express, Request, Response } from 'express'
+import { constants as HTTP_STATUS } from 'http2'
+import { PostProps } from '../../../../../domain/entities/post/post.props'
+import { ValidationError } from '../../../../../domain/errors/validation.error'
+import { CreateOriginalPostUseCaseFactory } from '../../../../factories/usecases/create-original-post.usecase.factory'
+import { CreateQuotePostUseCaseFactory } from '../../../../factories/usecases/create-quote-post.usecase.factory'
+import { CreateRepostPostUseCaseFactory } from '../../../../factories/usecases/create-repost-post.usecase.factory'
+import { WithErrorHandler } from '../../middlewares/error-handler'
+
+export class PostController {
+    constructor(app: Express) {
+        app.post('/posts', this.create.bind(this))
+    }
+
+    @WithErrorHandler()
+    async create(req: Request, res: Response) {
+        const { type } = req.body as PostProps
+
+        if (type === 'original') {
+            const post =
+                await CreateOriginalPostUseCaseFactory.create().execute(
+                    req.body
+                )
+
+            return res.status(HTTP_STATUS.HTTP_STATUS_OK).json({ post })
+        }
+
+        if (type === 'quote') {
+            const post = await CreateQuotePostUseCaseFactory.create().execute(
+                req.body
+            )
+
+            return res.status(HTTP_STATUS.HTTP_STATUS_OK).json({ post })
+        }
+        if (type === 'repost') {
+            const post = await CreateRepostPostUseCaseFactory.create().execute(
+                req.body
+            )
+
+            return res.status(HTTP_STATUS.HTTP_STATUS_OK).json({ post })
+        }
+
+        throw new ValidationError(['body.type should be string'])
+    }
+}
--- src/infrastructure/presentation/http/routes.ts
+++ src/infrastructure/presentation/http/routes.ts
@@ -1,4 +1,5 @@
 import express from 'express'
+import { PostController } from './controllers/posts/posts.controller'
 export const httpApp = express()
 
 httpApp.use(express.json())
@@ -6,5 +7,7 @@ httpApp.use(express.json())
 httpApp.use(express.urlencoded({ extended: true }))
 
 httpApp.get('/ping', (_req, res) => res.send('pong'))
+
+new PostController(httpApp)
 
 export default httpApp
