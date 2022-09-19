803d38c05cda8c3918782feac98784239548cc1b
Guilherme Cunha <guilherme.s.cunha@accenture.com>
2022-09-19 08:26:03 -0300

    feat: adding TypeormPostRepository

--- src/infrastructure/db/typeorm/repositories/post.repository.ts
+++ src/infrastructure/db/typeorm/repositories/post.repository.ts
@@ -1,3 +1,5 @@
+import { endOfDay, startOfDay } from 'date-fns'
+import { Between } from 'typeorm'
 import { PostProps } from '../../../../domain/entities/post/post.props'
 import { IPostRepository } from '../../../../domain/ports/repositories/post-repository'
 import { PostEntity } from '../entities/Post'
@@ -10,15 +12,34 @@ export class TypeormPostRepository implements IPostRepository {
     }
     async createPost(post: PostProps): Promise<PostProps> {
         const database = await this.database.getDatabase()
-
         const repository = database.getRepository(PostEntity)
 
-        repository.create(post as PostEntity)
+        const entity = await repository.create(post as PostEntity)
+        const savedEntity = await repository.save(entity)
+
+        return savedEntity as PostProps
     }
-    countPostsByUserInADay(userId: string, day: Date): Promise<number> {
-        throw new Error('Method not implemented.')
+    async countPostsByUserInADay(authorId: string, day: Date): Promise<number> {
+        const database = await this.database.getDatabase()
+        const repository = database.getRepository(PostEntity)
+
+        return await repository.count({
+            where: {
+                authorId,
+                createdAt: Between(startOfDay(day), endOfDay(day)),
+            },
+        })
     }
-    getPostById(postId: string): Promise<PostProps | null> {
-        throw new Error('Method not implemented.')
+    async getPostById(postId: string): Promise<PostProps | null> {
+        const database = await this.database.getDatabase()
+        const repository = database.getRepository(PostEntity)
+
+        const entity = await repository.findOne({
+            where: {
+                id: postId,
+            },
+        })
+
+        return entity as PostProps
     }
 }
