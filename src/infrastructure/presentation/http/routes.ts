437fc85e3ad83ad82f7145bb8a278bb32ac5f947
Guilherme Cunha <guiscunha@gmail.com>
2022-09-19 21:14:06 -0300

    feat: adding post create route

--- src/infrastructure/presentation/http/routes.ts
+++ src/infrastructure/presentation/http/routes.ts
@@ -1,5 +1,6 @@
-import express from 'express'
+import express, { NextFunction, Request, Response } from 'express'
 import { PostController } from './controllers/posts/posts.controller'
+import { errorHandler } from './middlewares/error-handler'
 export const httpApp = express()
 
 httpApp.use(express.json())
@@ -8,6 +9,17 @@ httpApp.use(express.urlencoded({ extended: true }))
 
 httpApp.get('/ping', (_req, res) => res.send('pong'))
 
-new PostController(httpApp)
+const postController = new PostController()
+
+httpApp.post('/posts', postController.create.bind(postController))
+
+httpApp.use(function (
+    err: Error,
+    req: Request,
+    res: Response,
+    next: NextFunction
+) {
+    return errorHandler(err, req, res, next)
+})
 
 export default httpApp
